243dbc0e9980f572dc15585c5235b0314943c2d5
ramcj7742 <ramcj7742@wrdsb.ca>
2020-09-16 13:45:20 +0000

    Finished driver license and voting problem

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
+ * This program will determine weather they can 
+ * drive or not
  * @author 
  */
 public class Main {
@@ -9,7 +12,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //constant for legal driving age
+    final int DRIVING_AGE = 16;
+    // constant for legal voting age
+    final int VOTING_AGE = 18;
+
+    // get users age 
+    System.out.println("Please enter your age:");
+    int age = input.nextInt();
+
+    // check to see if they are old enough to drive
+    if (age >= VOTING_AGE) {
+      System.out.println("You are old enough to drive!");
+      System.out.println("You are old enough to vote!");
+      else if (age >= DRIVING_AGE) {
+        System.out.println("You are old enough to drive!");
+        System.out.println("You are not old enough to vote");
+      }
+    } else {
+      System.out.println("Sorry, you are not old enough to drive!");
+      System.out.println("You also cant vote...:(");
+    }
     
   }
 }
